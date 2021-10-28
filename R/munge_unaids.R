59496d1814071ea0bca67080c3c6e34864869b2b
Karishma Srikanth <krs8rb@virginia.edu>
2021-10-28 16:50:02 -0400

    Addresses function issues to clean console output, fix bug bug in pepfar column, and improve UE

--- R/munge_unaids.R
+++ R/munge_unaids.R
@@ -1,10 +1,7 @@
 #' @title Cleaning UNAIDS Data
 #'
 #' @description This function cleans and fetches UNAIDS Estimates/Test and Treat Data
-#' @param sheet_id Google sheet ID with UN aids data
-#' @param tab Tab number corresponding to a set of indicators
-#' @param skipnum Number of lines to skip before reading
-#'
+#' @param return_type returns either HIV Estimates or Test & Treat Data
 #' @return
 #' @export
 #'
@@ -13,37 +10,45 @@
 #'    munge_unaids(sheet_id = googledrive::as_id('1tkwP532mPL_yy7hJuHNAHaZ1_K_wd7zo_8AjeOe7fRs'), tab = 3, skipnum = 4)
 #' }
 #'
-munge_unaids <- function(sheet_id, tab, skipnum) {
+munge_unaids <- function(return_type) {
 
   #to specify NA's when reading in data
   missing <- c("...", " ")
 
   # Get valid pepfar list
   pepfar_cntry <- glamr::get_outable() %>%
-    dplyr::filter(stringr::str_detect(operatingunit, "Region", negate = TRUE)) %>%
     dplyr::pull(countryname)
+
+  # Google Sheet ID to original
+  sheet_id <- googledrive::as_id("1tkwP532mPL_yy7hJuHNAHaZ1_K_wd7zo_8AjeOe7fRs")
 
   sheet_id_names <- "1vaeac7hb7Jb6RSaMcxLXCeTyim3mtTcy-a1DQ6JooCw"
 
-  gdrive_df <- googlesheets4::read_sheet(sheet_id, sheet = tab, skip = skipnum, na = missing) %>%
-    dplyr::rename(year = !!names(.[1]),
-                  iso =  !!names(.[2]),
-                  country =  !!names(.[3]))
-
   #HIV estimates tab - specific
-  if (tab == 1) {
+  if (return_type == "HIV Estimates") {
 
-    hiv_est_names <- googlesheets4::read_sheet(sheet_id, range = "HIV estimates - by Year!A5:AY5") %>%
-      dplyr::rename(year = !!names(.[1]),
-                    iso =  !!names(.[2]),
-                    country =  !!names(.[3]))
+    gdrive_df <- suppressMessages(
+      googlesheets4::read_sheet(sheet_id, sheet = 1, skip = 5, na = missing) %>%
+        dplyr::rename(year = !!names(.[1]),
+                      iso =  !!names(.[2]),
+                      country =  !!names(.[3]))
+    )
 
-    names_cw <- googlesheets4::read_sheet(sheet_id_names) %>%
-      dplyr::filter(sheet == "HIV estimates - by Year") %>%
-      dplyr::select(-sheet) %>%
-      tidyr::pivot_wider(names_from = names,
-                         values_from = names_original) %>%
-      dplyr::select(-value)
+    hiv_est_names <- suppressMessages(
+      googlesheets4::read_sheet(sheet_id, range = "HIV estimates - by Year!A5:AY5") %>%
+        dplyr::rename(year = !!names(.[1]),
+                      iso =  !!names(.[2]),
+                      country =  !!names(.[3]))
+    )
+
+    names_cw <- suppressMessages(
+      googlesheets4::read_sheet(sheet_id_names) %>%
+        dplyr::filter(sheet == "HIV estimates - by Year") %>%
+        dplyr::select(-sheet) %>%
+        tidyr::pivot_wider(names_from = names,
+                           values_from = names_original) %>%
+        dplyr::select(-value)
+    )
 
     #change column names - stop if length of names is not the same as length of df
     stopifnot(ncol(names_cw) == ncol(gdrive_df))
@@ -52,28 +57,43 @@ munge_unaids <- function(sheet_id, tab, skipnum) {
   }
 
   #Test & Treat tab - specific
-  if (tab == 3) {
-    hiv_tt_names <- googlesheets4::read_sheet(sheet_id, sheet = tab, range = "HIV Test & Treat - by Year !A5:DF5") %>%
-      dplyr::rename(year = !!names(.[1]),
-             iso =  !!names(.[2]),
-             country =  !!names(.[3]))
+  if (return_type == "Test & Treat") {
 
-    names_cw_tt <- googlesheets4::read_sheet(sheet_id_names, sheet = 2) %>%
-      dplyr::filter(sheet == "HIV Test & Treat") %>%
-      dplyr::select(-sheet) %>%
-      tidyr::pivot_wider(names_from = names,
-                  values_from = names_original) %>%
-      dplyr::select(-value)
+    gdrive_df <- suppressMessages(
+      googlesheets4::read_sheet(sheet_id, sheet = 3, skip = 4, na = missing) %>%
+        dplyr::rename(year = !!names(.[1]),
+                      iso =  !!names(.[2]),
+                      country =  !!names(.[3]))
+    )
+
+    hiv_tt_names <- suppressMessages(
+      googlesheets4::read_sheet(sheet_id, sheet = 3, range = "HIV Test & Treat - by Year !A5:DF5") %>%
+        dplyr::rename(year = !!names(.[1]),
+                      iso =  !!names(.[2]),
+                      country =  !!names(.[3]))
+    )
+
+    names_cw_tt <- suppressMessages(
+      googlesheets4::read_sheet(sheet_id_names, sheet = 2) %>%
+        dplyr::filter(sheet == "HIV Test & Treat") %>%
+        dplyr::select(-sheet) %>%
+        tidyr::pivot_wider(names_from = names,
+                           values_from = names_original) %>%
+        dplyr::select(-value)
+    )
 
     #change column names - stop if length of names is not the same as length of df
     stopifnot(ncol(names_cw_tt) == ncol(gdrive_df))
 
     names(gdrive_df) <- names(names_cw_tt)
 
-    gdrive_df <-  gdrive_df %>%
-      dplyr::mutate(across(tidyselect:::where(is.list), ~na_if(., "NULL"))) %>%
-      dplyr::slice(-c(1,2)) %>%
-      dplyr::mutate_at(dplyr::vars(4:110), as.numeric)
+    gdrive_df <-  suppressWarnings(
+      gdrive_df %>%
+        dplyr::mutate(across(tidyselect:::where(is.list), ~na_if(., "NULL"))) %>%
+        dplyr::slice(-c(1,2)) %>%
+        dplyr::mutate_at(dplyr::vars(4:110), as.numeric)
+    )
+
   }
 
 
@@ -88,7 +108,7 @@ munge_unaids <- function(sheet_id, tab, skipnum) {
     tidyr::separate(indicator, sep = "_", into = c("indicator", "age", "sex", "stat"))
 
   #HIV estimates tab - specific munging
-  if (tab == 1) {
+  if (return_type == "HIV Estimates") {
     gdrive_df_clean <- gdrive_df_clean %>%
       dplyr::mutate(sheet = "HIV Estimates",
                     sex = ifelse(indicator == "pmtct", "female", sex),
@@ -110,7 +130,7 @@ munge_unaids <- function(sheet_id, tab, skipnum) {
   }
 
   #Test and Treat tab - specific munging
-  if (tab == 3) {
+  if (return_type == "Test & Treat") {
     gdrive_df_clean <- gdrive_df_clean %>%
       dplyr::mutate(sheet = "HIV Test & Treat",
                     sex = ifelse(indicator == "pmtct", "female", sex),
@@ -145,14 +165,16 @@ munge_unaids <- function(sheet_id, tab, skipnum) {
            pepfar = ifelse(country %in% pepfar_cntry, "PEPFAR", "Non-PEPFAR"))
 
   # To fix the formatting, let's return a list of tibbles w/ percentages in one and integers in the other
-  hiv_est <- gdrive_df_clean %>%
-    dplyr::group_split(indic_type) %>%
-    rlang::set_names(dplyr::group_keys(gdrive_df_clean %>% dplyr::group_by(indic_type)) %>%
-                       unlist() %>%
-                       stringr::str_remove(., "_indics") %>%
-                       stringr::str_c(" indicators") %>%
-                       stringr::str_to_title()) %>%
-    purrr::map(., ~.x %>% dplyr::mutate(value = as.numeric(value)))
+  hiv_est <- suppressWarnings(
+    gdrive_df_clean %>%
+      dplyr::group_split(indic_type) %>%
+      rlang::set_names(dplyr::group_keys(gdrive_df_clean %>% dplyr::group_by(indic_type)) %>%
+                         unlist() %>%
+                         stringr::str_remove(., "_indics") %>%
+                         stringr::str_c(" indicators") %>%
+                         stringr::str_to_title()) %>%
+      purrr::map(., ~.x %>% dplyr::mutate(value = as.numeric(value)))
+  )
 
   return(hiv_est)
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onLoad <- function (libname, pkgname)
 {
 
-  packageStartupMessage("\nTo pull HIV estimates, use tab = 1, sheet = 5;
-To pull Test & Treat, use tab = 3, sheet = 4\n")
+  packageStartupMessage("\nTo pull HIV estimates, use return_type == 'HIV Estimates';
+To pull Test & Treat, use return_type == 'Test & Treat'\n")
 
 
   invisible ()
