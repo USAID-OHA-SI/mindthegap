33bf3e9db1042af542c9c2467f8f4d4851e521a2
Karishma Srikanth <krs8rb@virginia.edu>
2021-11-04 11:05:35 -0400

    Changed loading message to include the year of the estimates and specify the function

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function (libname, pkgname)
 {
 
-  packageStartupMessage("\nTo pull HIV estimates, use return_type == 'HIV Estimates';
+  packageStartupMessage("\nTo pull UNAIDS 2021 estimates from 1990-2020, use munge_aids(). For HIV estimates, use return_type == 'HIV Estimates';
 To pull Test & Treat, use return_type == 'Test & Treat'\n")
 
 
