0b6fb9be099b3326fea42740ab15fe763ef4e6b3
Karishma Srikanth <krs8rb@virginia.edu>
2021-11-10 15:57:55 -0500

    add source info as an object

---
+++ R/source_info.R
@@ -0,0 +1,10 @@
+#' Title
+#' UNAIDS Source Info
+#' @description
+#' Returns UNAIDS Source info for consistent sourcing notes
+#'
+#' @export
+#'
+
+source_note <- "2021 AIDSinfo Global data on HIV epidemiology and response from UNAIDS"
+
