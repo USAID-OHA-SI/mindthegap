f8bc63c61a07c2a7301de63e794e4efabdbdbf34
Karishma Srikanth <krs8rb@virginia.edu>
2021-11-16 17:02:06 -0500

    push just PEPFAR dataset to gdrive

--- data-raw/push_2021_UNAIDS_estimates.R
+++ data-raw/push_2021_UNAIDS_estimates.R
@@ -11,18 +11,19 @@ library(tidyverse)
 data_type <- c("HIV Estimates", "HIV Estimates", "Test & Treat", "Test & Treat")
 ind_type <- c("Integer", "Percent", "Integer", "Percent")
 
+#make a list of the 4 dataframes
 lst <- purrr::map2( .x = data_type,
                     .y = ind_type,
                     .f = ~munge_unaids(.x, .y)
 )
 
+#FULL DATASET -----------------------------
+
 #add file to drive
 gs_id_new <- drive_create(name = "UNAIDS 2021 Clean Estimates", path = "SI Folder/Analysis, Data & Tools/UNAIDS", type = "spreadsheet")
 
-
 #specify sheet names
 tab_names <- c("HIV Estimates - Integer", "HIV Estimates - Percent", "Test & Treat - Integer", "Test & Test - Percent")
-
 
 #sheet names
 purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_new), sheet = .x))
@@ -30,6 +31,20 @@ purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_new), sheet = .x)
 #read the data in!
 purrr::walk2(.x = lst, .y = tab_names, .f = ~sheet_write(data = .x, ss = gs_id_new, sheet = .y))
 
+#JUST PEPFAR DATASET -----------------------------
 
+#make a separate list filtering to just PEPFAR
+list_pepfar <- lapply(lst, function(x) filter(x, pepfar == "PEPFAR"))
 
+#add file to drive
+gs_id_pepfar <- drive_create(name = "PEPFAR Only - UNAIDS 2021 Clean Estimates", path = "SI Folder/Analysis, Data & Tools/UNAIDS", type = "spreadsheet")
+
+#specify sheet names
+tab_names <- c("HIV Estimates - Integer", "HIV Estimates - Percent", "Test & Treat - Integer", "Test & Test - Percent")
+
+#sheet names
+purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_pepfar), sheet = .x))
+
+#read the data in!
+purrr::walk2(.x = list_pepfar, .y = tab_names, .f = ~sheet_write(data = .x, ss = gs_id_pepfar, sheet = .y))
 
