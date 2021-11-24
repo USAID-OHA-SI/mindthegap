590d72304d9bda50b97e90113feb85c22556ac7d
Karishma Srikanth <krs8rb@virginia.edu>
2021-11-24 14:25:20 -0500

    annotate gs_id script to identify which ids correspond to which files

--- R/gs_id.R
+++ R/gs_id.R
@@ -5,11 +5,16 @@
 #' @keywords internal
 #'
 gs_id <- '1Ui1r5ynn9xYky86hHMO9kmdNZdWmANI5nAQ1N2b0wdM'
+#old mindthegap sheet
 
 gs_id_unaids <- googledrive::as_id("1tkwP532mPL_yy7hJuHNAHaZ1_K_wd7zo_8AjeOe7fRs")
+#original UNAIDS data
 
 gs_id_names <- "1vaeac7hb7Jb6RSaMcxLXCeTyim3mtTcy-a1DQ6JooCw"
+#UNAIDS crosswalk
 
 gs_clean_id <- "1Vt54N2W51WEWY0XlOr6vP4pa3F-7AwpXxyRx9Y9Hcq4"
+#Clean UNAIDS estimates data on google drive
 
 drive_id <- googledrive::as_id("1-iCrHGyU-xfDmzdfgXJ1P_wLI90s5RR-")
+#UNAIDS drive folder
