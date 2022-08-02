20a6e79158c1528ce4fdde64483c30ad8ccb6199
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-02 16:41:09 -0400

    update with 2021 estimates google id

--- R/gs_id.R
+++ R/gs_id.R
@@ -8,7 +8,10 @@ gs_id <- '1Ui1r5ynn9xYky86hHMO9kmdNZdWmANI5nAQ1N2b0wdM'
 #old mindthegap sheet
 
 gs_id_unaids <- googledrive::as_id("1tkwP532mPL_yy7hJuHNAHaZ1_K_wd7zo_8AjeOe7fRs")
-#original UNAIDS data
+#original UNAIDS data -2020 Estimates
+
+gs_id_unaids_2021 <- googledrive::as_id("1J20strrRqwIpH65n9YLTuOd99lCip2Wb_I3SOnXzMgw")
+#original UNAIDS data - 2021 Estimates
 
 gs_id_names <- "1vaeac7hb7Jb6RSaMcxLXCeTyim3mtTcy-a1DQ6JooCw"
 #UNAIDS crosswalk
