592643c59322572aa86120b6272c844c4f35757f
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-03 08:35:47 -0400

    update push file for 2022 estimates

--- data-raw/push_2021_UNAIDS_estimates.R
+++ data-raw/push_2021_UNAIDS_estimates.R
@@ -27,17 +27,17 @@ df_tt <- munge_unaids("HIV Test & Treat", "Percent")
 
 df_est_lim <- df_est %>%
   filter(indicator %in% c("PLHIV", "AIDS Related Deaths", "New HIV Infections"),
-         age == "all",
-         sex == "all",
-         stat == "est") %>%
-  select(year, country, iso, indicator, value)
+         age == "All",
+         sex == "All") %>%
+  select(year, country, iso, indicator, estimate)
 
 #reshape wide to align with T&T
 df_est_lim <- df_est_lim %>%
   pivot_wider(names_from = indicator,
+              values_from = estimate,
               names_glue = "{indicator %>% str_extract_all('Deaths|Infections|PLHIV') %>% tolower}")
 
-#plhiv for reference
+  #plhiv for reference
 df_plhiv <- df_est_lim %>%
   filter(year == max(year)) %>%
   select(year, country, iso, plhiv)
@@ -64,20 +64,19 @@ goal <- 90
 df_tt_lim <- df_tt %>%
   filter(year == max(year),
          indicator %in% c("KNOWN_STATUS", "PLHIV_ON_ART", "VLS"),
-         age == "all",
-         sex == "all",
-         stat == "est") %>%
-  select(year, country, iso, indicator, value)
+         age == "All",
+         sex == "All") %>%
+  select(year, country, iso, indicator, estimate)
 
 plhiv_base_90 <- df_tt_lim %>%
-  filter(!is.na(value)) %>%
+  filter(!is.na(estimate)) %>%
   mutate(indicator = recode(indicator, "KNOWN_STATUS" = "Known\nStatus",
                             "PLHIV_ON_ART" = "On\nART"),
          set = recode(indicator, "Known\nStatus" = 1,
                       "On\nART" = 2,
                       "VLS" = 3),
          goal_rate = round((goal/100)^set*100),
-         achv = value >= goal_rate) %>%
+         achv = estimate >= goal_rate) %>%
   count(country,iso, achv) %>%
   mutate(plhiv_base_90s = ifelse(achv == TRUE & n == 3, TRUE, FALSE)) %>%
   distinct(country, iso, plhiv_base_90s)
@@ -88,13 +87,12 @@ plhiv_base_90 <- df_tt_lim %>%
 df_tt_rel_lim <- df_tt %>%
   filter(year == max(year),
          indicator %in% c("KNOWN_STATUS", "KNOWN_STATUS_ON_ART", "ON_ART_VLS"),
-         age == "all",
-         sex == "all",
-         stat == "est") %>%
-  select(year, country, iso, indicator, value)
+         age == "All",
+         sex == "All") %>%
+  select(year, country, iso, indicator, estimate)
 
 rel_base_90 <- df_tt_rel_lim %>%
-  filter(!is.na(value)) %>%
+  filter(!is.na(estimate)) %>%
   mutate(indicator = recode(indicator, "KNOWN_STATUS" = "Known\nStatus",
                             "KNOWN_STATUS_ON_ART" = "On\nART",
                             "ON_ART_VLS" = "VLS"),
@@ -102,7 +100,7 @@ rel_base_90 <- df_tt_rel_lim %>%
                       "On\nART" = 2,
                       "VLS" = 3),
          # goal_rate = round((goal/100)^set*100),
-         achv = value >= goal) %>%
+         achv = estimate >= goal) %>%
   group_by(country) %>%
   count(country, iso, achv) %>%
   mutate(rel_base_90s = ifelse(achv == TRUE & n == 3, TRUE, FALSE)) %>%
@@ -146,17 +144,27 @@ purrr::walk2(.x = unaids_list, .y = tab_names, .f = ~sheet_write(data = .x, ss =
 #JUST PEPFAR DATASET -----------------------------
 
 #make a separate list filtering to just PEPFAR
-list_pepfar <- lapply(unaids_list, function(x) filter(x, pepfar == "PEPFAR"))
+list_pepfar <- lapply(unaids_list, function(x) filter(x, pepfar == TRUE))
+
+#append 4 dfs to one df
+df_pepfar <- do.call("rbind", list_pepfar)
 
 #add file to drive
-gs_id_pepfar <- drive_create(name = "PEPFAR Only - UNAIDS 2021 Clean Estimates", path = "SI Folder/Analysis, Data & Tools/UNAIDS", type = "spreadsheet")
+gs_id_pepfar <- drive_create(name = "PEPFAR Only - UNAIDS 2022 Clean Estimates", path = "SI Folder/Analysis, Data & Tools/UNAIDS", type = "spreadsheet")
 
 #specify sheet names
-tab_names <- c("HIV Estimates - Integer", "HIV Estimates - Percent", "Test & Treat - Integer", "Test & Treat - Percent")
+tab_names <- c("2022 UNAIDS Estimates")
 
 #sheet names
 purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_pepfar), sheet = .x))
 
 #read the data in!
-purrr::walk2(.x = list_pepfar, .y = tab_names, .f = ~sheet_write(data = .x, ss = gs_id_pepfar, sheet = .y))
+sheet_write(data = df_pepfar, ss = gs_id_pepfar, sheet = tab_names)
+
+
+    # #this is to read in the data with multiple tabs
+    # tab_names <- c("HIV Estimates - Integer", "HIV Estimates - Percent", "Test & Treat - Integer", "Test & Treat - Percent")
+    # purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_pepfar), sheet = .x))
+    # purrr::walk2(.x = df_pepfar, .y = tab_names, .f = ~sheet_write(data = .x, ss = gs_id_pepfar, sheet = .y))
+
 
