c2df2ab300bb3d61ad594f3858aeddfd6316c6ee
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-09 17:22:30 -0400

    update push to gdrive script

--- data-raw/push_2021_UNAIDS_estimates.R
+++ data-raw/push_2021_UNAIDS_estimates.R
@@ -26,7 +26,7 @@ df_tt <- munge_unaids("HIV Test & Treat", "Percent")
 # EPI CONTROL FLAG: INFECTIONS + DEATHS
 
 df_est_lim <- df_est %>%
-  filter(indicator %in% c("PLHIV", "AIDS Related Deaths", "New HIV Infections"),
+  filter(indicator %in% c("Number PLHIV", "Number AIDS Related Deaths", "Number New HIV Infections"),
          age == "All",
          sex == "All") %>%
   select(year, country, iso, indicator, estimate)
@@ -63,15 +63,16 @@ goal <- 90
 
 df_tt_lim <- df_tt %>%
   filter(year == max(year),
-         indicator %in% c("KNOWN_STATUS", "PLHIV_ON_ART", "VLS"),
+         indicator %in% c("Percent Known Status of PLHIV", "Percent on ART of PLHIV", "Percent VLS of PLHIV"),
          age == "All",
          sex == "All") %>%
   select(year, country, iso, indicator, estimate)
 
 plhiv_base_90 <- df_tt_lim %>%
   filter(!is.na(estimate)) %>%
-  mutate(indicator = recode(indicator, "KNOWN_STATUS" = "Known\nStatus",
-                            "PLHIV_ON_ART" = "On\nART"),
+  mutate(indicator = recode(indicator, "Percent Known Status of PLHIV" = "Known\nStatus",
+                            "Percent on ART of PLHIV" = "On\nART",
+                            "Percent VLS of PLHIV" = "VLS"),
          set = recode(indicator, "Known\nStatus" = 1,
                       "On\nART" = 2,
                       "VLS" = 3),
@@ -79,23 +80,24 @@ plhiv_base_90 <- df_tt_lim %>%
          achv = estimate >= goal_rate) %>%
   count(country,iso, achv) %>%
   mutate(plhiv_base_90s = ifelse(achv == TRUE & n == 3, TRUE, FALSE)) %>%
-  distinct(country, iso, plhiv_base_90s)
+  distinct(country, iso, plhiv_base_90s) %>%
+  rename(`Achieved 90s with PLHIV base in 2021` = plhiv_base_90s)
 
 #90'S PROGRESS - RELATIVE CASCADE BASE
 
 #Relative Base
 df_tt_rel_lim <- df_tt %>%
   filter(year == max(year),
-         indicator %in% c("KNOWN_STATUS", "KNOWN_STATUS_ON_ART", "ON_ART_VLS"),
+         indicator %in% c("Percent Known Status of PLHIV", "Percent on ART with Known Status", "Percent VLS on ART"),
          age == "All",
          sex == "All") %>%
   select(year, country, iso, indicator, estimate)
 
 rel_base_90 <- df_tt_rel_lim %>%
   filter(!is.na(estimate)) %>%
-  mutate(indicator = recode(indicator, "KNOWN_STATUS" = "Known\nStatus",
-                            "KNOWN_STATUS_ON_ART" = "On\nART",
-                            "ON_ART_VLS" = "VLS"),
+  mutate(indicator = recode(indicator, "Percent Known Status of PLHIV" = "Known\nStatus",
+                            "Percent on ART with Known Status" = "On\nART",
+                            "Percent VLS on ART" = "VLS"),
          set = recode(indicator, "Known\nStatus" = 1,
                       "On\nART" = 2,
                       "VLS" = 3),
@@ -105,12 +107,13 @@ rel_base_90 <- df_tt_rel_lim %>%
   count(country, iso, achv) %>%
   mutate(rel_base_90s = ifelse(achv == TRUE & n == 3, TRUE, FALSE)) %>%
   # select(-c(achv, n)) %>%
-  distinct(country, iso, rel_base_90s)
+  distinct(country, iso, rel_base_90s) %>%
+rename(`Achieved 90s with relative base in 2021` = rel_base_90s)
 
 #COMBINE 3 FLAGS INTO ONE DF AND LIST
 
-flag_list <- full_join(plhiv_base_90, rel_base_90, by = c("country", "iso"))
-flag_list <- full_join(flag_list, df_epi_cntry, by = c("country", "iso")) %>% list()
+flag_list <- full_join(plhiv_base_90, rel_base_90, by = c("country", "iso")) %>% list()
+#flag_list <- full_join(flag_list, df_epi_cntry, by = c("country", "iso")) %>% list()
 
 #PUSH TO DRIVE -----------------------------------------------------------------------
 
@@ -130,16 +133,22 @@ unaids_list <- flatten(unaids_list)
 #FULL DATASET -----------------------------
 
 #add file to drive
-gs_id_new <- drive_create(name = "UNAIDS 2021 Clean Estimates", path = "SI Folder/Analysis, Data & Tools/UNAIDS", type = "spreadsheet")
+gs_id_new <- drive_create(name = "UNAIDS 2022 Clean Estimates", path = "SI Folder/Analysis, Data & Tools/UNAIDS", type = "spreadsheet")
 
 #specify sheet names
-tab_names <- c("HIV Estimates - Integer", "HIV Estimates - Percent", "Test & Treat - Integer", "Test & Treat - Percent")
+#tab_names <- c("HIV Estimates - Integer", "HIV Estimates - Percent", "Test & Treat - Integer", "Test & Treat - Percent")
 
-#sheet names
-purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_new), sheet = .x))
+#append 4 dfs to one df
+df_unaids <- do.call("rbind", unaids_list)
 
-#read the data in!
-purrr::walk2(.x = unaids_list, .y = tab_names, .f = ~sheet_write(data = .x, ss = gs_id_new, sheet = .y))
+sheet_write(data = df_unaids, ss = gs_id_new, sheet = tab_names)
+
+
+# #sheet names
+# purrr::walk(.x = tab_names, .f = ~sheet_add(as_sheets_id(gs_id_new), sheet = .x))
+#
+# #read the data in!
+# purrr::walk2(.x = unaids_list, .y = tab_names, .f = ~sheet_write(data = .x, ss = gs_id_new, sheet = .y))
 
 #JUST PEPFAR DATASET -----------------------------
 
