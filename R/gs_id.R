b662b22f8030ac8cec2e1f2de5872b7a6cf088aa
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-18 23:57:17 -0400

    archive old ids

--- R/gs_id.R
+++ R/gs_id.R
@@ -4,11 +4,6 @@
 #' @return google sheet id
 #' @keywords internal
 #'
-gs_id <- '1Ui1r5ynn9xYky86hHMO9kmdNZdWmANI5nAQ1N2b0wdM'
-#old mindthegap sheet
-
-gs_id_unaids_2020 <- googledrive::as_id("1tkwP532mPL_yy7hJuHNAHaZ1_K_wd7zo_8AjeOe7fRs")
-#original UNAIDS data -2020 Estimates
 
 gs_id_unaids <- googledrive::as_id("1J20strrRqwIpH65n9YLTuOd99lCip2Wb_I3SOnXzMgw")
 #original UNAIDS data - 2021 Estimates
@@ -19,14 +14,20 @@ gs_id_names <- "1vaeac7hb7Jb6RSaMcxLXCeTyim3mtTcy-a1DQ6JooCw"
 gs_clean_id <- "1k5j169a_MtecrMdsu_n870Y0Y8tezLPopULn7AP3lQQ"
 #Clean UNAIDS estimates data on google drive
 
-gs_clean_id_2020 <- "1Vt54N2W51WEWY0XlOr6vP4pa3F-7AwpXxyRx9Y9Hcq4"
-#Clean UNAIDS estimates data on google drive
-
 pepfar_clean_id <- "1ZfxOScjuLnoGiXcsmfNnWn4EsqtxyFrvql8ERqdWSjk"
-#PEPFAR only Clean UNAIDS data on google drive
-
-pepfar_clean_id_2020 <- "14GBwcmAuGHETKknbJjeN1JkWSunpHcT4DvM3q4TlRIM"
 #PEPFAR only Clean UNAIDS data on google drive
 
 drive_id <- googledrive::as_id("1-iCrHGyU-xfDmzdfgXJ1P_wLI90s5RR-")
 #UNAIDS drive folder
+
+#OLD IDS --------------------------------------------
+
+
+gs_id <- '1Ui1r5ynn9xYky86hHMO9kmdNZdWmANI5nAQ1N2b0wdM'
+#old mindthegap sheet
+
+pepfar_clean_id_2020 <- "14GBwcmAuGHETKknbJjeN1JkWSunpHcT4DvM3q4TlRIM"
+#PEPFAR only Clean UNAIDS data on google drive
+
+gs_id_unaids_2020 <- googledrive::as_id("1tkwP532mPL_yy7hJuHNAHaZ1_K_wd7zo_8AjeOe7fRs")
+#original UNAIDS data -2020 Estimates
