116fdccfebc647741dd3b2e4d9ca02dc4569b6ce
Aaron Chafetz <achafetz@usaid.gov>
2023-10-13 16:20:52 -0400

    feat: wrapper around pull_unaids, resolves #45

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,5 @@
 # mindthegap 1.1
+* Create wrappers around `pull_unaids`, `pull_estimates` and `pull_testtreat` to  minimize typing and improve clarity [2022-10-13]
 * Duplicate release with the "latest" tag to minimizing confusion and manual updating in future [2022-10-13]
 * Quiet output for user when running `pull_unaids` [2022-10-13]
 
--- R/pull_unaids.R
+++ R/pull_unaids.R
@@ -1,4 +1,4 @@
-#' @title Pull clean UNAIDS 2022 Estimates
+#' @title Pull clean UNAIDS 2022 HIV Estimates or Test & Treat
 #'
 #' @description Pull clean UNAIDS 2022 (1990-2022) estimates
 #'
@@ -49,4 +49,45 @@ pull_unaids <- function(data_type, pepfar_only = TRUE) {
     dplyr::filter(sheet == data_type)
 
   return(df)
+}
+
+
+#' Pull clean UNAIDS HIV Estimates
+#'
+#' Pull clean UNAIDS 2022 (1990-2022) estimates data. Wrapper around
+#' `pull_unaids`.
+#'
+#' @inheritParams pull_unaids
+#'
+#' @return df
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    pull_estimates()
+#' }
+pull_estimates <- function(pepfar_only = TRUE){
+
+  pull_unaids("HIV Estimates", pepfar_only)
+
+}
+
+#' Pull clean UNAIDS HIV Test & Treat
+#'
+#' Pull clean UNAIDS 2022 (1990-2022) HIV Test & Treat data. Wrapper around
+#' `pull_unaids`.
+#'
+#' @inheritParams pull_unaids
+#'
+#' @return df
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    pull_testtreat()
+#' }
+pull_testtreat <- function(pepfar_only = TRUE){
+
+  pull_unaids("HIV Test & Treat", pepfar_only)
+
 }
